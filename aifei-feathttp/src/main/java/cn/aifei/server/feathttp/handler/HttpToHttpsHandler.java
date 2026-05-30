c3cadf874ff0f44db6c4df7c520f7d951fa924f8
CrazyAirhead <l4qiang@gmail.com>
2026-05-30 10:44:46 +0800

    feat: 增加aifei-feathttp

---
+++ aifei-feathttp/src/main/java/cn/aifei/server/feathttp/handler/HttpToHttpsHandler.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2011-2035 詹波 (aifei.cn)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.aifei.server.feathttp.handler;
+
+import cn.aifei.server.feathttp.FeatHttpConfig;
+import cn.aifei.server.feathttp.ssl.SslConfig;
+import tech.smartboot.feat.core.server.HttpHandler;
+import tech.smartboot.feat.core.server.HttpRequest;
+import tech.smartboot.feat.core.server.HttpResponse;
+import tech.smartboot.feat.core.common.HttpStatus;
+
+/**
+ * http 请求重定向到 https
+ *
+ * 配置方法：
+ * feathttp.http.toHttps = true
+ *
+ * 重定向默认使用状态码 302，可配置状态码：
+ * feathttp.http.toHttpsStatusCode=301
+ */
+public class HttpToHttpsHandler implements HttpHandler {
+
+    protected String httpsPrefix;
+    protected int statusCode;
+    protected FeatHttpConfig config;
+
+    public HttpToHttpsHandler(FeatHttpConfig config) {
+        this.config = config;
+        this.statusCode = config.getHttpToHttpsStatusCode();
+    }
+
+    @Override
+    public void handle(HttpRequest request) throws Throwable {
+        HttpResponse response = request.getResponse();
+        String httpsUrl = buildRedirectHttpsUrl(request);
+
+        response.setHttpStatus(statusCode, getStatusText(statusCode));
+        response.setHeader("Location", httpsUrl);
+        response.setHeader("Connection", "close");
+        response.close();
+    }
+
+    protected String buildRedirectHttpsUrl(HttpRequest request) {
+        if (httpsPrefix == null) {
+            buildUrlPrefix();
+        }
+
+        String uri = request.getRequestURI();
+        String queryString = request.getQueryString();
+        if (queryString != null && queryString.length() > 0) {
+            StringBuilder ret = new StringBuilder(httpsPrefix.length() + uri.length() + 1 + queryString.length());
+            ret.append(httpsPrefix).append(uri).append('?').append(queryString);
+            return ret.toString();
+        } else {
+            StringBuilder ret = new StringBuilder(httpsPrefix.length() + uri.length());
+            ret.append(httpsPrefix).append(uri);
+            return ret.toString();
+        }
+    }
+
+    protected void buildUrlPrefix() {
+        SslConfig sslConfig = config.getSslConfig();
+        String ret = "https://localhost";
+
+        if (sslConfig.getPort() != 443) {
+            ret = ret + ":" + sslConfig.getPort();
+        }
+
+        this.httpsPrefix = ret;
+    }
+
+    private String getStatusText(int code) {
+        switch (code) {
+            case 301: return "Moved Permanently";
+            case 302: return "Found";
+            case 303: return "See Other";
+            case 307: return "Temporary Redirect";
+            case 308: return "Permanent Redirect";
+            default: return "Found";
+        }
+    }
+}
---
+++ aifei-feathttp/src/main/java/cn/aifei/server/feathttp/ssl/SslConfig.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2011-2035 詹波 (aifei.cn)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.aifei.server.feathttp.ssl;
+
+import cn.aifei.util.Prop;
+import cn.aifei.util.StrUtil;
+
+/**
+ * SslConfig
+ */
+public class SslConfig {
+
+    static final String SSL_ENABLE                = "feathttp.ssl.enable";
+    static final String SSL_PORT                  = "feathttp.ssl.port";
+    static final String SSL_PROTOCOL              = "feathttp.ssl.protocol";
+
+    static final String SSL_KEY_STORE_TYPE        = "feathttp.ssl.keyStoreType";
+    static final String SSL_KEY_STORE             = "feathttp.ssl.keyStore";
+    static final String SSL_KEY_STORE_PASSWORD    = "feathttp.ssl.keyStorePassword";
+
+    static final String SSL_KEY_ALIAS             = "feathttp.ssl.keyAlias";
+    static final String SSL_KEY_PASSWORD          = "feathttp.ssl.keyPassword";
+
+    static final String SSL_CIPHERS               = "feathttp.ssl.ciphers";
+    static final String SSL_ENABLED_PROTOCOLS     = "feathttp.ssl.enabledProtocols";
+
+    // ---------
+
+    protected boolean enable                      = false;
+    protected int port                            = 443;
+    protected String protocol                     = "TLS";
+
+    protected String keyStoreType;
+    protected String keyStore;
+    protected String keyStorePassword;
+
+    protected String keyAlias;
+    protected String keyPassword;
+
+    protected String[] ciphers = null;
+    protected String[] enabledProtocols = null;
+
+    public SslConfig(Prop p) {
+        enable                = p.getBoolean(SSL_ENABLE, enable);
+        port                  = p.getInt(SSL_PORT, port);
+        protocol              = p.get(SSL_PROTOCOL, protocol);
+
+        keyStoreType          = p.get(SSL_KEY_STORE_TYPE);
+        keyStore              = p.get(SSL_KEY_STORE);
+        keyStorePassword      = p.get(SSL_KEY_STORE_PASSWORD);
+
+        keyAlias              = p.get(SSL_KEY_ALIAS);
+        keyPassword           = p.get(SSL_KEY_PASSWORD);
+
+        if (StrUtil.notBlank(p.get(SSL_CIPHERS))) {
+            ciphers = p.get(SSL_CIPHERS)
+                    .replace("  ", " ").replace("  ", " ").replace("  ", " ")
+                    .replace(" ", "")
+                    .split(":");
+        }
+
+        if (StrUtil.notBlank(p.get(SSL_ENABLED_PROTOCOLS))) {
+            enabledProtocols = p.get(SSL_ENABLED_PROTOCOLS)
+                    .replace("  ", " ").replace("  ", " ").replace("  ", " ")
+                    .split(" ");
+        }
+
+        if (enable) {
+            checkConfig();
+        }
+    }
+
+    protected void checkConfig() {
+        if (StrUtil.isBlank(keyStore)) {
+            throw new IllegalStateException(SSL_KEY_STORE + " can not be blank");
+        }
+        if (StrUtil.isBlank(keyStorePassword)) {
+            throw new IllegalStateException(SSL_KEY_STORE_PASSWORD + " can not be blank");
+        }
+    }
+
+    public boolean isEnable() {
+        return enable;
+    }
+
+    public void setEnable(boolean enable) {
+        this.enable = enable;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public String getKeyAlias() {
+        return keyAlias;
+    }
+
+    public void setKeyAlias(String keyAlias) {
+        this.keyAlias = keyAlias;
+    }
+
+    public String getKeyPassword() {
+        return keyPassword;
+    }
+
+    public void setKeyPassword(String keyPassword) {
+        this.keyPassword = keyPassword;
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }
+
+    public String getKeyStorePassword() {
+        return keyStorePassword;
+    }
+
+    public void setKeyStorePassword(String keyStorePassword) {
+        this.keyStorePassword = keyStorePassword;
+    }
+
+    public String getKeyStoreType() {
+        return keyStoreType;
+    }
+
+    public void setKeyStoreType(String keyStoreType) {
+        this.keyStoreType = keyStoreType;
+    }
+
+    public String[] getCiphers() {
+        return ciphers;
+    }
+
+    public void setCiphers(String[] ciphers) {
+        this.ciphers = ciphers;
+    }
+
+    public String[] getEnabledProtocols() {
+        return enabledProtocols;
+    }
+
+    public void setEnabledProtocols(String[] enabledProtocols) {
+        this.enabledProtocols = enabledProtocols;
+    }
+}
